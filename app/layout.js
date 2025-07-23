b13fa0b01ac16f668a834af565d458a050cd648c
Dana Coe <dana.coe@bitlox.com>
2025-07-23 19:42:57 +0800

    More sideways locking

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,14 @@ export const metadata = {
   description: "Take a trip on Solana",
 };
 
-export const viewport = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';  // Moved here as a separate export
+export const viewport = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body className="overscroll-x-none overflow-x-hidden">  {/* Added these classes */}
+        {children}
+      </body>
     </html>
   );
 }
