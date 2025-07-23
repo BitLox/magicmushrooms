a2bc79f9fd8425d4176797b5511fc30bca891423
Dana Coe <dana.coe@bitlox.com>
2025-07-23 19:38:09 +0800

    Another viewport try

--- app/(landing)/_components/Hero.js
+++ app/(landing)/_components/Hero.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="h-dvh flex items-center justify-center relative">
-      <div className="bg-[url(/hero.png)] bg-center bg-cover bg-no-repeat fixed top-0 left-0 h-full w-full -z-10"></div>
-      <h1 className="text-6xl md:text-8xl font-bold text-white glow-text z-10 text-center">  {/* Added text-center */}
+    <div className="h-dvh flex items-center justify-center relative">  {/* Already relative, good */}
+      <div className="bg-[url(/hero.png)] bg-center bg-cover bg-no-repeat absolute top-0 left-0 h-full w-full -z-10"></div>  {/* Changed fixed to absolute */}
+      <h1 className="text-6xl md:text-8xl font-bold text-white glow-text z-10 text-center">
         Magic Mushrooms Coin
       </h1>
     </div>
