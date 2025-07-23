7488a1fa64ff146b40a224dbdd0545362a4fde9c
Dana Coe <dana.coe@bitlox.com>
2025-07-23 18:09:17 +0800

    Added header text

--- app/(landing)/_components/Hero.js
+++ app/(landing)/_components/Hero.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="h-dvh">
+    <div className="h-dvh flex items-center justify-center relative">  {/* Added flex centering and relative for layering */}
       <div className="bg-[url(/hero.png)] bg-center bg-cover bg-no-repeat fixed top-0 left-0 h-full w-full -z-10"></div>
+      <h1 className="text-6xl font-bold text-white glow-text z-10">  {/* Added classes for size, color, glow, and z-index */}
+        Magic Mushroom Coin
+      </h1>
     </div>
   );
 };
