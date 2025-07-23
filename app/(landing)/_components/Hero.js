b9d7ba0d8f483c6e63e616cab5ddccdf8e39a934
Dana Coe <dana.coe@bitlox.com>
2025-07-23 18:16:18 +0800

    Text sixe increased

--- app/(landing)/_components/Hero.js
+++ app/(landing)/_components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="h-dvh flex items-center justify-center relative">  {/* Added flex centering and relative for layering */}
       <div className="bg-[url(/hero.png)] bg-center bg-cover bg-no-repeat fixed top-0 left-0 h-full w-full -z-10"></div>
-      <h1 className="text-6xl font-bold text-white glow-text z-10">  {/* Added classes for size, color, glow, and z-index */}
+      <h1 className="text-8xl font-bold text-white glow-text z-10">  {/* Added classes for size, color, glow, and z-index */}
         Magic Mushroom Coin
       </h1>
     </div>
