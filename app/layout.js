61a283a17e0f985ee84ae6bfe35f673a877c8e78
Dana Coe <dana.coe@bitlox.com>
2025-07-23 19:31:26 +0800

    Fixed meta viewport

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Magic Mushrooms | Solana",
   description: "Take a trip on Solana",
-  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',  // This locks out zooming
 };
+
+export const viewport = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';  // Moved here as a separate export
 
 export default function RootLayout({ children }) {
   return (
